a3e572e5de717bb2ac66e5d91f1cb998d365cc22
Ihor Dmytriv <dmytriwihor@gmail.com>
2025-06-19 18:19:04 +0200

    Implement the script for getting & printing weather info for Paris city

--- app/main.py
+++ app/main.py
@@ -1,6 +1,29 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    response = requests.get(
+        "https://api.weatherapi.com/v1/current.json?q=Paris&key=" + API_KEY
+    )
+
+    if response.status_code == 200:
+        json_data = response.json()
+
+        city_location_data = json_data["location"]
+        for key, value in city_location_data.items():
+            print(f"{key}: {value}")
+
+        city_weather_data = json_data["current"]
+        for key, value in city_weather_data.items():
+            print(f"{key}: {value}")
+    else:
+        print("Error:", response.status_code, response.text)
 
 
 if __name__ == "__main__":
